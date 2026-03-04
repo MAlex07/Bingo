584fbc3c3793b9ee79060abe391faa1eaf200412
Petrik Diak <diak@petrik.hu>
2026-03-04 08:52:36 +0100

    nevek.txt be lett olvasva

---
+++ Bingo/BingoJatekos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+	internal class BingoJatekos
+	{
+		private List<string> nev;
+		private char[,] kartya;
+
+		public List<string> Nev { get => nev; set => nev = value; }
+		public char[,] Kartya { get => kartya; set => kartya = value; }
+
+		public BingoJatekos(List<string> nev, char[,] kartya)
+		{
+			this.nev = nev;
+			this.kartya = kartya;
+		}
+
+		private void Beolvas(string fajlnev)
+		{
+			foreach (var sor in File.ReadAllLines(fajlnev))
+			{
+				this.nev.Add(sor);
+			}
+		}
+	}
+}
